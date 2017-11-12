2eb4ae3e505df4e1a24b69d1b8e0d677a3450e20
lbynum <luciusbynum@gmail.com>
2017-11-12 14:11:49 -0800

    fix duration

--- preprocess_wireless_data.R
+++ preprocess_wireless_data.R
@@ -33,9 +33,37 @@ preprocess_wireless_data <- function(x) {
   wireless$disconnect_month <- as.numeric(wireless$disconnect_month)
   wireless$disconnect_minute <- as.numeric(wireless$disconnect_minute)
   
-  # drop rows with negative duration
-  numeric_data <- select_if(wireless, is.numeric)
-  non_numeric_data <- select_if(wireless, is.character)
+  # fix duration column
+  wireless <- wireless %>% mutate(
+    start_datetime = paste(
+      connect_day, 
+      connect_month, 
+      connect_year, 
+      connect_hour, 
+      connect_minute, 
+      0,  
+      sep = '-') %>% dmy_hms(),
+    stop_datetime = paste(
+      disconnect_day, 
+      disconnect_month, 
+      disconnect_year, 
+      # replace midnight with 24 if disconnect at midnight
+      ifelse(disconnect_hour == 0 & connect_hour != 0, 24, disconnect_hour),
+      disconnect_minute, 
+      0,  
+      sep = '-') %>% dmy_hms()
+  )
+  # find problematic examples
+  is_incorrect_duration <- which(wireless$stop_datetime < wireless$start_datetime)
+  # increment disconnect day for examples where duration is wrong
+  wireless$stop_datetime[is_incorrect_duration] <-  
+    wireless$stop_datetime[is_incorrect_duration] + days(1)
   
+  # create new duration_mins
+  wireless <- wireless %>% mutate(
+    duration_mins = as.numeric(
+      difftime(stop_datetime, start_datetime, units = 'min')
+    )
+  )
   return(wireless)
 }
