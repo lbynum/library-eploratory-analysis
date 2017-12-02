745a5aa6bab3606c729a401b41f67b6def03f703
lbynum <luciusbynum@gmail.com>
2017-12-02 12:12:14 -0800

    read in csvs

--- preprocess_ezproxy_data.R
+++ preprocess_ezproxy_data.R
@@ -1,23 +1,34 @@
 library(lubridate)
 library(dplyr)
 
-preprocess_ezproxy_data <- function(x) {
+preprocess_ezproxy_data <- function() {
   ezproxy_data_path <- 'HMC\ Math\ 158/ezproxy/'
   
-  # read in all files
+  # read in all tsv files
   filelist = paste0(ezproxy_data_path, list.files(ezproxy_data_path, '*.tsv'))
   is_a_ccl_file <- endsWith(filelist, 'hashed.tsv')
   ccl_files <- filelist[is_a_ccl_file]
   spu_files <- filelist[!is_a_ccl_file]
-  spu <- lapply(spu_files, function(x) read.delim(x, header = TRUE, sep = '\t'))
-  ccl <- lapply(ccl_files, function(x) read.delim(x, header = TRUE, sep = '\t'))
+  spu <- lapply(spu_files, 
+                function(x) read.delim(x, header = TRUE, sep = '\t'))
+  ccl <- lapply(ccl_files, 
+                function(x) read.delim(x, header = TRUE, sep = '\t'))
+  
+  # read in all csv files (note headers added manually)
+  csv_filelist = paste0(ezproxy_data_path, 
+                        list.files(ezproxy_data_path, '*.csv'))
+  ccl_csv <- lapply(csv_filelist, 
+                    function(x) read.csv(x, header = TRUE, strip.white = TRUE))
+  
+  # append csv data frames to tsv data frame list
+  ccl <- c(ccl, ccl_csv)
   
   # concatenate files while aligning columns properly
   spu <- do.call(rbind, 
                  lapply(spu, function(x) x[match(names(spu[[1]]), names(x))]))
   ccl <- do.call(rbind, 
                  lapply(ccl, function(x) x[match(names(ccl[[1]]), names(x))]))
-  
+
   # fix ccl datetime column (split into two columns and convert to date obj.)
   ccl$session_start <- substr(ccl$datetime, start = 1, stop = 19)
   ccl$session_end <- substr(ccl$datetime, start = 21, stop = 40)
